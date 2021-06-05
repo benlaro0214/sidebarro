e851cfa6e067a5aede70c45aafae6ff47fb45eb3
Ben Larocque <benlaro0214@hotmail.com>
2021-06-05 11:30:27 -0400

    Initial upload

---
+++ button.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "70px";
+    document.getElementById("main").style.marginLeft = "70px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
